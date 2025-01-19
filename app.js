80c46ebbf6e79b3b12c2677df04e96766f0b2208
Gabriel de Oliveira Cardoso <108769102+Cardosogoc@users.noreply.github.com>
2025-01-19 15:39:21 -0300

    Revert "Alterando a quantidade para 50."
    This reverts commit ece2173e81c527b766f90c501a8269dba5f6ac76.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaNumerosSorteados = []
-let numeroLimite = 50;
+let numeroLimite = 10;
 let numeroSecreto = gerarUmNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function mensagemInicial() {
   exibirTextoNaTela("h1", "Jogo do Número Secreto");
-  exibirTextoNaTela("p", "Escolha um número entre 1 e 50:");
+  exibirTextoNaTela("p", "Escolha um número entre 1 e 10:");
   
 }
 
